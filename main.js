83ead8fabd8738fbe58fa529a598d007f3324db5
sarahfalter <sarah.falter@ufl.edu>
2021-04-06 13:30:18 -0400

    Added Character Bio Page, slider, and flip animation - SF

---
+++ main.js
@@ -0,0 +1,30 @@
+
+    $(document).ready(function(){
+          $('.content-slider').slick({
+            centerMode: true,
+            centerPadding: '0',
+            slidesToShow: 1,
+            responsive: [
+              {
+                breakpoint: 768,
+                settings: {
+                  arrows: true,
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 1
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  arrows: true,
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 1,
+                  swipe: true,
+                  swipeToSlide: true
+                }
+              }
+            ]
+      });
+    });
