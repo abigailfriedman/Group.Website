c3902f2af4c3a305c5f2df624e182ff212458f20
sarahfalter <sarah.falter@ufl.edu>
2021-04-06 23:22:41 -0400

    Added images and fixed slider styling - SF

--- js/main.js
+++ js/main.js
@@ -15,3 +15,33 @@ var granimInstance = new Granim({
         }
     }
 });
+
+    $(document).ready(function(){
+          $('.content-slider').slick({
+            centerMode: true,
+            centerPadding: '0',
+            slidesToShow: 1,
+            responsive: [
+              {
+                breakpoint: 768,
+                settings: {
+                  arrows: true,
+                  centerMode: true,
+                  centerPadding: '0',
+                  slidesToShow: 1
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  arrows: false,
+                  centerMode: true,
+                  centerPadding: '0',
+                  slidesToShow: 1,
+                  swipe: true,
+                  swipeToSlide: true
+                }
+              }
+            ]
+      });
+    });
